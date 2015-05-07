62d18b72cfce6bfa26ab541056d2e1f577456c13
zhengfaxiang <fxzheng0906@outlook.com>
2015-05-07 10:26:03 +0800

    fixed some warnings and edited capture templates

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,62 +1,65 @@
-(global-set-key "\C-cC" 'org-capture)
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 
+(global-set-key "\C-cC" 'org-capture)
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
-(setq org-directory "~/org/")
-(setq org-default-notes-file "~/.notes.org")
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/todo.org" "Tasks")
+(defvar org-directory "~/org/")
+(defvar org-default-notes-file "~/.notes.org")
+(defvar org-capture-templates
+  '(("t" "Todo" entry (file+headline "~/org/todo.org" "Tasks")
 	 "* TODO %?\n %i\n %a")
-	("b" "Bug" entry (file+datetree "~/org/bug.org" "Bugs")
+	("b" "Bug" entry (file+headline "~/org/bug.org" "Bugs")
 	 "* %?\nEntered on %U\n %i\n %a")
+    ("n" "Note" entry (file+headline "~/org/notes.org" "Notes")
+     "* %?\nEntered on %U\n %i\n %a")
 	("j" "Journal" entry (file+datetree "~/org/journal.org" "Journal")
 	 "* %?\nEntered on %U\n %i\n %a")))
 
-(setq org-startup-indented t
-      org-log-done 'time
-      org-log-done 'note
-      org-completion-use-ido t
-      org-edit-timestamp-down-means-later t
-      org-agenda-start-on-weekday nil
-      org-agenda-span 14
-      org-agenda-include-diary t
-      org-agenda-window-setup 'curent-window
-      org-fast-tag-selection-single-key 'expert
-      org-tags-column 80
-      org-agenda-inhibit-startup t
-      org-agenda-use-tag-inheritance nil
-      )
+(defvar org-startup-indented t)
+(defvar org-log-done 'time)
+(defvar org-log-done 'note)
+(defvar org-completion-use-ido t)
+(defvar org-edit-timestamp-down-means-later t)
+(defvar org-agenda-start-on-weekday nil)
+(defvar org-agenda-span 14)
+(defvar org-agenda-include-diary t)
+(defvar org-agenda-window-setup 'curent-window)
+(defvar org-fast-tag-selection-single-key 'expert)
+(defvar org-tags-column 80)
+(defvar org-agenda-inhibit-startup t)
+(defvar org-agenda-use-tag-inheritance nil)
+
 
 ; Refile targets include this file and any file contributing to the agenda - up to 5 levels deep
-(setq org-refile-targets (quote ((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))))
+(defvar org-refile-targets (quote ((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))))
 ; Targets start with the file name - allows creating level 1 tasks
-(setq org-refile-use-outline-path (quote file))
+(defvar org-refile-use-outline-path (quote file))
 ; Targets complete in steps so we start with filename, TAB shows the next level of targets etc
-(setq org-outline-path-complete-in-steps t)
+(defvar org-outline-path-complete-in-steps t)
 
 
-(setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
-              (sequence "WAITING(w@/!)" "SOMEDAY(S)" "|" "CANCELLED(c@/!)"))))
+(defvar org-todo-keywords
+      (quote ((sequence "TODO(t)" "|" "DONE(d)")
+              (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
+              (sequence "|" "CANCELLED(c)"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Org clock
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Save the running clock and all clock history when exiting Emacs, load it on startup
-(setq org-clock-persistence-insinuate t)
-(setq org-clock-persist t)
-(setq org-clock-in-resume t)
+(defvar org-clock-persistence-insinuate t)
+(defvar org-clock-persist t)
+(defvar org-clock-in-resume t)
 
 ;; Change task state to STARTED when clocking in
-(setq org-clock-in-switch-to-state "STARTED")
+(defvar org-clock-in-switch-to-state "STARTED")
 ;; Save clock data and notes in the LOGBOOK drawer
-(setq org-clock-into-drawer t)
+(defvar org-clock-into-drawer t)
 ;; Removes clocked tasks with 0:00 duration
-(setq org-clock-out-remove-zero-time-clocks t)
+(defvar org-clock-out-remove-zero-time-clocks t)
 
 ;; Show the clocked-in task - if any - in the header line
 (defun sanityinc/show-org-clock-in-header-line ()
@@ -78,16 +81,16 @@
    '(progn
       (require 'org-clock)
       ; @see http://irreal.org/blog/?p=671
-      (setq org-src-fontify-natively t)
+      (defvar org-src-fontify-natively t)
       (defun soft-wrap-lines ()
         "Make lines wrap at window edge and on word boundary,
         in current buffer."
         (interactive)
-        (setq truncate-lines nil)
-        (setq word-wrap t)
+        (defvar truncate-lines nil)
+        (defvar word-wrap t)
         )
       (add-hook 'org-mode-hook '(lambda ()
-                                  (setq evil-auto-indent nil)
+                                  (defvar evil-auto-indent nil)
                                   (soft-wrap-lines)
                                   ))))
 
@@ -113,3 +116,4 @@
      (sqlite . t)))))
 
 (provide 'init-org)
+;;; init-org.el ends here
