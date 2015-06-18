c4fdeb48c620c0ffd69d900d90c7f0d7f3b35590
zhengfaxiang <fxzheng0906@outlook.com>
2015-06-19 02:51:58 +0800

    fixed some warnings

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -10,12 +10,12 @@
 (if (commandp 'ido-vertical-mode)
     (progn
       (ido-vertical-mode 1)
-      (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)))
+      (setq-default ido-vertical-define-keys 'C-n-C-p-up-down-left-right)))
 ;; http://sachachua.com/blog/2014/03/emacs-basics-call-commands-name-m-x-tips-better-completion-using-ido-helm/
 
 (if (commandp 'flx-ido-mode)
     (flx-ido-mode 1))
-(setq ido-enabble-flex-matching t)
+(setq-default ido-enabble-flex-matching t)
 (setq ido-use-faces nil)
 (setq ido-use-filename-at-point 'guess)
 (setq ido-auto-merge-work-directories-length 0)
