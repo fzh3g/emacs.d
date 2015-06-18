4a8ad890fcf7127e67c1e2fc1f0edba69b10abbf
zhengfaxiang <fxzheng0906@outlook.com>
2015-06-19 02:45:30 +0800

    added company-statistics

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -12,13 +12,15 @@
 (setq-default company-transformaers 'company-sort-by-occurence)
 
 (add-hook 'company-mode-hook
-	  '(lambda ()
-	    (add-to-list 'company-backends 'company-math-symbols-unicode)))
+          '(lambda ()
+             (require 'company-math)
+             (add-to-list 'company-backends 'company-math-symbols-unicode)
+             (require 'company-statistics)
+             (company-statistics-mode)))
 
 (defun my:company-for-latex ()
   (require 'company-auctex)
   (company-auctex-init)
-  (require 'company-math)
   (add-to-list 'company-backends 'company-math-symbols-latex))
 
 (defun my:company-for-c ()
@@ -26,13 +28,10 @@
   (add-to-list 'company-backends 'company-irony)
   (add-to-list 'company-backends 'company-c-headers))
 
-(defun my:company-for-python ()
-  (setq company-backends (delete 'company-ropemacs company-backends)))
 
 (add-hook 'TeX-mode-hook 'my:company-for-latex)
 (add-hook 'c-mode-hook 'my:company-for-c)
 (add-hook 'c++-mode-hook 'my:company-for-c)
-(add-hook 'python-mode-hook 'my:company-for-python)
-
+(add-hook 'cmake-mode-hook 'my:company-for-c)
 
 (provide 'init-company)
