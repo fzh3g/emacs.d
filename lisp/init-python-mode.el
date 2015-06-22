d768d12370e07a606524b0532c9254a3f2e04750
zhengfaxiang <fxzheng0906@outlook.com>
2015-06-23 01:07:58 +0800

    anaconda mode

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,28 +1,25 @@
-;;;
-;;----------------------------------------------------------------------
-;; elpy
-;;----------------------------------------------------------------------
-(require 'elpy)
-(elpy-enable)
-(elpy-use-ipython)
-(setq elpy-rpc-backend "jedi")
-(add-hook 'pyvenv-post-activate-hook 'pyvenv-restart-python)
+;; anaconda mode
+(require 'anaconda-mode)
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (anaconda-mode)
+             (setq electric-indent-chars (delq ?: electric-indent-chars))
+             ))
 
 ;;----------------------------------------------------------------------
 ;; python shell
 ;;----------------------------------------------------------------------
-;(require 'python)
-;(setq
-; python-shell-interpreter "ipython"
-; python-shell-interpreter-args " "
-; python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-; python-shell-prompt-output-regexp "Out\\[[0-9]+]: "
-; python-shell-completion-setup-code
-;   "from IPython.core.completerlib import module_completion"
-; python-shell-completion-module-string-code
-;   "';'.join(module_completion('''%s'''))\n"
-; python-shell-completion-string-code
-;   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+(setq-default
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args " "
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 
 (provide 'init-python-mode)
