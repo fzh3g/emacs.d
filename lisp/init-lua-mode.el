25ec1fd3a1134e61ef3f303fecc6c7a2b3214e96
zhengfaxiang <fxzheng0906@outlook.com>
2015-06-23 00:31:15 +0800

    lua indent

--- lisp/init-lua-mode.el
+++ lisp/init-lua-mode.el
@@ -1,14 +1,12 @@
 (defun my-lua-mode-setup ()
   (interactive)
-  (setq-local imenu-generic-expression '(("Variable" "^ *\\([a-zA-Z0-9_.]+\\) *= *{ *[^ ]*$" 1)
-                                         ("Function" "function +\\([^ (]+\\).*$" 1)
-                                         ("Module" "^ *module +\\([^ ]+\\) *$" 1)
-                                         ("Variable" "^ *local +\\([^ ]+\\).*$" 1)))
-  (setq safe-local-variable-values
-        '((lua-indent-level . 2)
-          (lua-indent-level . 3)
-          (lua-indent-level . 4)
-          (lua-indent-level . 8))))
+  (setq-default lua-indent-level 4)
+  (setq-local imenu-generic-expression
+              '(("Variable" "^ *\\([a-zA-Z0-9_.]+\\) *= *{ *[^ ]*$" 1)
+                ("Function" "function +\\([^ (]+\\).*$" 1)
+                ("Module" "^ *module +\\([^ ]+\\) *$" 1)
+                ("Variable" "^ *local +\\([^ ]+\\).*$" 1))))
+
 
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
