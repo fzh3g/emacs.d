5c461a697622684a93af13c15c5edbdef3099c7e
zhengfaxiang <fxzheng0906@outlook.com>
2015-10-07 11:11:11 +0800

    init-scratch-message

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -8,7 +8,7 @@
 
 (setq initial-major-mode 'text-mode)
 (setq-default initial-scratch-message
-              (concat "Welcome to Emacs ~ Happy hacking ~~ *★,°*:.☆\(￣▽￣)/$:*.°★* \n"))
+              (concat "Welcome to Emacs ~ Happy hacking ~~"))
 
 ;; http://andrewjamesjohnson.com/suppressing-ad-handle-definition-warnings-in-emacs/
 (setq ad-redefinition-action 'accept)
