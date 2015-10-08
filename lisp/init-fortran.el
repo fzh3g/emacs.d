357b47b32807cb41a7ae16e778e45e4e945b28d4
zhengfaxiang <fxzheng0906@outlook.com>
2015-10-08 23:19:47 +0800

    disabled flycheck for fortran and f90 mode

--- lisp/init-fortran.el
+++ lisp/init-fortran.el
@@ -1,13 +1,17 @@
 (use-package fortran-mode
   :defer t
-  :mode "\\.\\(f\\|F\\)$")
+  :mode "\\.\\(f\\|F\\)$"
+  :init
+  (add-hook 'fortran-mode-hook
+            '(lambda ()
+               (flycheck-mode -1))))
 
 (use-package f90-mode
   :defer t
   :mode "\\.\\(f90\\|F90\\|f95\\|F95\\|g90\\|g95\\)$"
-  :init
-  (add-hook 'f90-mode-hook
-            '(lambda ()
-               (abbrev-mode 1))))
+  :init (add-hook 'f90-mode-hook
+                  '(lambda ()
+                     (abbrev-mode 1)
+                     (flycheck-mode -1))))
 
 (provide 'init-fortran)
