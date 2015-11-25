d81fef02ba070589008569103cfb02afbbd25ca2
Faxiang Zheng <fxzheng0906@outlook.com>
2015-11-26 00:50:19 +0800

    fci-mode

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -13,18 +13,6 @@
                   company-begin-commands '(self-insert-command)
                   company-auto-complete nil
                   company-tooltip-align-annotations t)
-
-    (defun company-turn-off-fci (&rest ignore)
-      (when (boundp 'fci-mode)
-        (setq company-fci-mode-on-p fci-mode)
-        (when fci-mode (fci-mode -1))))
-
-    (defun company-maybe-turn-on-fci (&rest ignore)
-      (when company-fci-mode-on-p (fci-mode 1)))
-
-    (add-hook 'company-completion-started-hook 'company-turn-off-fci)
-    (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
-    (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
     
     ;; Nicer looking faces
     (custom-set-faces
