85ecbec7c383e3dd58d7e427ae0b1d63dcede9ae
Faxiang Zheng <fxzheng0906@outlook.com>
2015-11-27 15:39:54 +0800

    company mode

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,18 +1,36 @@
 (use-package company
   :defer t
   :init
-  (add-hook 'after-init-hook 'global-company-mode)
-  :config
   (progn
-    (setq-default company-idle-delay 0
+    (setq-default company-idle-delay 0.2
                   company-minimum-prefix-length 2
                   company-require-match nil
                   company-dabbrev-ignore-case nil
                   company-dabbrev-downcase nil
                   company-show-numbers t
-                  company-begin-commands '(self-insert-command)
                   company-auto-complete nil
-                  company-tooltip-align-annotations t)))
+                  company-frontends '(company-pseudo-tooltip-frontend)
+                  company-tooltip-align-annotations t))
+
+  (defvar-local company-fci-mode-on-p nil)
+  (add-hook 'after-init-hook 'global-company-mode)
+  (defun company-turn-off-fci (&rest ignore)
+    (when (boundp 'fci-mode)
+      (setq company-fci-mode-on-p fci-mode)
+      (when fci-mode (fci-mode -1))))
+
+  (defun company-maybe-turn-on-fci (&rest ignore)
+    (when company-fci-mode-on-p (fci-mode 1)))
+  ;; Nicer looking faces
+  (custom-set-faces
+   '(company-tooltip-common
+     ((t (:inherit company-tooltip :weight bold :underline nil))))
+   '(company-tooltip-common-selection
+     ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
+
+  (add-hook 'company-completion-started-hook 'company-turn-off-fci)
+  (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+  (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci))
 
 (use-package company-statistics
   :defer t
