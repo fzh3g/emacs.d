93aaa73ec439e762e70f6332c0fac8dc176a02a1
Faxiang Zheng <fxzheng0906@outlook.com>
2015-12-16 01:43:40 +0800

    better key for avy

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -176,9 +176,9 @@
 ;; avy
 (use-package avy
   :bind
-  (("C-:" . avy-goto-char)
-   ("C-'" . avy-goto-char-2)
-   ("M-g f" . avy-goto-line)
+  (("C-;" . avy-goto-char)
+   ("C-'" . avy-goto-line)
+   ("M-g f" . avy-goto-char-2)
    ("M-g w" . avy-goto-subword-1)))
 
 (use-package multiple-cursors
