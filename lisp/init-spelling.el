cca3791c5615000e5965d9d2e8d99ef4c695d21f
Faxiang Zheng <fxzheng0906@outlook.com>
2015-12-15 23:57:41 +0800

    init-spelling.el change setq-default to setq

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -5,17 +5,17 @@
     (dolist (hook '(text-mode-hook TeX-mode-hook org-mode-hook markdown-mode-hook))
       (add-hook hook 'flyspell-mode))
     ;; better performance
-    (setq-default flyspell-issue-message-flag nil)
+    (setq flyspell-issue-message-flag nil)
     (cond
      ((executable-find "aspell")
-      (setq-default ispell-program-name "aspell"))
+      (setq ispell-program-name "aspell"))
      ((executable-find "hunspell")
-      (setq-default ispell-program-name "hunspell")
+      (setq ispell-program-name "hunspell")
       ;; just reset dictionary to the safe one "en_US" for hunspell.
       ;; if we need use different dictionary, we specify it in command line arguments
-      (setq-default ispell-local-dictionary "en_US")
-      (setq-default ispell-local-dictionary-alist
+      (setq ispell-local-dictionary "en_US")
+      (setq ispell-local-dictionary-alist
                     '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8))))
-     (t (setq-default ispell-program-name nil)))))
+     (t (setq ispell-program-name nil)))))
 
 (provide 'init-spelling)
