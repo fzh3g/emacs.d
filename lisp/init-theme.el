9263f3e0be95194ab4dc85cbaee191b47640df90
Faxiang Zheng <fxzheng0906@outlook.com>
2016-01-02 16:51:43 +0800

    deleted tomorrow theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -4,18 +4,4 @@
             '(lambda ()
                (load-theme 'dracula t))))
 
-;(use-package color-theme-sanityinc-tomorrow
-;  :init
-;  (defun light ()
-;    "Activate a light color theme."
-;    (interactive)
-;    (color-theme-sanityinc-tomorrow-day))
-;
-;  (defun dark ()
-;    "Activate a dark color theme."
-;    (interactive)
-;    (color-theme-sanityinc-tomorrow-night))
-;
-;  (add-hook 'after-init-hook 'color-theme-sanityinc-tomorrow-night))
-
 (provide 'init-theme)
