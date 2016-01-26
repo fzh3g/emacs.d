99102d02194e4acaa9ba96554c912492be8689c5
Faxiang Zheng <fxzheng0906@outlook.com>
2016-01-26 22:46:26 +0800

    indent code

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -1,21 +1,21 @@
 ; http://www.emacswiki.org/emacs/ShellMode
 (global-set-key (kbd "C-x t") 'alt-shell-dwim)
 (defun alt-shell-dwim (arg)
-   "Run an inferior shell like `shell'. If an inferior shell as its I/O
+  "Run an inferior shell like `shell'. If an inferior shell as its I/O
  through the current buffer, then pop the next buffer in `buffer-list'
  whose name is generated from the string \"*shell*\". When called with
  an argument, start a new inferior shell whose I/O will go to a buffer
  named after the string \"*shell*\" using `generate-new-buffer-name'."
-   (interactive "P")
-   (let* ((shell-buffer-list
- 	  (let (blist)
-	     (dolist (buff (buffer-list) blist)
-	       (when (string-match "^\\*shell\\*" (buffer-name buff))
-	 	(setq blist (cons buff blist))))))
-	  (name (if arg
-	 	   (generate-new-buffer-name "*shell*")
-	 	 (car shell-buffer-list))))
-     (shell name)))
+  (interactive "P")
+  (let* ((shell-buffer-list
+          (let (blist)
+            (dolist (buff (buffer-list) blist)
+              (when (string-match "^\\*shell\\*" (buffer-name buff))
+                (setq blist (cons buff blist))))))
+         (name (if arg
+                   (generate-new-buffer-name "*shell*")
+                 (car shell-buffer-list))))
+    (shell name)))
 
 ;; ansi color
 (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
