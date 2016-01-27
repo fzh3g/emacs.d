f6e50d36be3d0a8842adc42fe51ca53f2011dd8d
Faxiang Zheng <fxzheng0906@outlook.com>
2016-01-27 18:43:24 +0800

    delete a duplicate bracket

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -22,7 +22,7 @@
     (when *win32*
       (setenv "GIT_ASKPASS" "git-gui--askpass"))
 
-    (global-set-key (kbd "M-<f12>") 'magit-status))
+    (global-set-key (kbd "M-<f12>") 'magit-status)
     (global-set-key (kbd "C-x g") 'magit-status))
   :config
   (progn
