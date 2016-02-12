752f7b904466ca13642f666f2ba777d82abff340
Faxiang Zheng <fxzheng0906@outlook.com>
2016-02-13 01:45:08 +0800

    visible bell

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -4,7 +4,7 @@
               case-fold-search t
               compilation-scroll-output t
               set-mark-command-repeat-pop t
-              visible-bell nil
+              visible-bell t
               delete-selection-mode t)
 
 ; http://www.gnu.org/software/emacs/manual/html_node/emacs/General-VC-Options.html
