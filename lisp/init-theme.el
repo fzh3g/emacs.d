f603d0b2a283d676a6ac9f3c35108efac12e352e
Faxiang Zheng <fxzheng0906@outlook.com>
2016-02-15 14:19:47 +0800

    interactively load light/dark-theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -20,6 +20,16 @@
   :init
   (add-hook 'after-init-hook
             '(lambda ()
-               (load-theme 'sanityinc-tomorrow-night t))))
+               (load-theme 'sanityinc-tomorrow-night t)))
+  :config
+  (progn
+    (defun light-theme ()
+      "Load a light color theme."
+      (interactive)
+      (load-theme 'sanityinc-tomorrow-day t))
+    (defun dark-theme ()
+      "Load a dark color theme."
+      (interactive)
+      (load-theme 'sanityinc-tomorrow-night t))))
 
 (provide 'init-theme)
