53454554b3c20be18b5da5b7a5e94b885a1070d3
Faxiang Zheng <fxzheng0906@outlook.com>
2016-02-24 15:44:04 +0800

    company mode for matlab

--- lisp/init-matlab.el
+++ lisp/init-matlab.el
@@ -1,4 +1,12 @@
 (use-package matlab-load
-  :defer t)
+  :defer t
+  :init
+  (progn
+    (defun fx/init-matlab ()
+      (make-variable-buffer-local 'company-backends)
+      (add-to-list 'company-backends
+                   '(company-matlab-shell :with company-yasnippet)))
+    (dolist (hook '(matlab-mode-hook matlab-shell-mode-hook))
+      (add-hook hook 'fx/init-matlab))))
 
 (provide 'init-matlab)
