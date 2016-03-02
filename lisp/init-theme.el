c4409a8aa436a30f90ed0cd25d653a6032b011cc
Faxiang Zheng <fxzheng0906@outlook.com>
2016-03-02 18:42:45 +0800

    powerline-reset after load-theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -2,16 +2,20 @@
   :init
   (add-hook 'after-init-hook
             '(lambda ()
-               (load-theme 'sanityinc-tomorrow-night t)))
+               (load-theme 'sanityinc-tomorrow-day t)))
   :config
   (progn
     (defun light-theme ()
       "Load a light color theme."
       (interactive)
-      (load-theme 'sanityinc-tomorrow-day t))
+      (load-theme 'sanityinc-tomorrow-day t)
+      (when (fboundp 'powerline-reset)
+        (powerline-reset)))
     (defun dark-theme ()
       "Load a dark color theme."
       (interactive)
-      (load-theme 'sanityinc-tomorrow-night t))))
+      (load-theme 'sanityinc-tomorrow-night t)
+      (when (fboundp 'powerline-reset)
+        (powerline-reset)))))
 
 (provide 'init-theme)
