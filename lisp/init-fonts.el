e834ae6b2731642c1d881d768f35dea227b7137d
Faxiang Zheng <fxzheng0906@outlook.com>
2016-03-03 23:07:02 +0800

    font and scrach

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,21 +1,20 @@
 ;; change font for better looking text
 (cond
  (*linux*
-  (set-face-attribute 'default nil :font "Monaco 11"))
+  (set-face-attribute 'default nil :font "Monaco for Powerline 11"))
  (*is-a-mac*
-  (set-face-attribute 'default nil :font "Monaco 14"))
+  (set-face-attribute 'default nil :font "Monaco for Powerline 14"))
  (t
-  (set-face-attribute 'default nil :font "Monaco 12")))
+  (set-face-attribute 'default nil :font "Monaco for Powerline 12")))
 ;; Chinese Font
-;; (dolist (charset '(han cjk-misc bopomofo))
+;; (dolist (charset '(kana han cjk-misc bopomofo))
 ;;   (set-fontset-font (frame-parameter nil 'font)
 ;;                     charset
 ;;                     (font-spec :family "STKaiti")))
 ;; change font size
-(when (display-graphic-p)
-  (global-set-key (kbd "C-M-=") 'text-scale-increase)
-  (global-set-key (kbd "C-M--") 'text-scale-decrease)
-  (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
-  (global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease))
+(global-set-key (kbd "C-M-=") 'text-scale-increase)
+(global-set-key (kbd "C-M--") 'text-scale-decrease)
+(global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
+(global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
 
 (provide 'init-fonts)
