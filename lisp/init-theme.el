371dac3d086eee9f93f5b0d7bd2ca514e1fd0727
Faxiang Zheng <fxzheng0906@outlook.com>
2016-04-22 15:39:43 +0800

    modeline color

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -3,7 +3,7 @@
   (progn
     (setq moe-light-pure-white-background-in-terminal t)
     (moe-dark)
-    (moe-theme-set-color 'blue)
+    (moe-theme-set-color 'purple)
     (powerline-moe-theme)))
 
 (provide 'init-theme)
