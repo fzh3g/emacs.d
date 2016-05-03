b882fdbaf61aa3ed7bc3ff982030004842d996e0
Faxiang Zheng <fxzheng0906@outlook.com>
2016-05-04 03:20:42 +0800

    better font

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,16 +1,10 @@
 ;; change font for better looking text
-(cond
- (*linux*
-  (set-face-attribute 'default nil :font "Monaco 11"))
- (*is-a-mac*
-  (set-face-attribute 'default nil :font "Monaco 14"))
- (t
-  (set-face-attribute 'default nil :font "Monaco 12")))
-;; Chinese Font
+(set-face-attribute 'default nil :font "Monaco:pixelsize=14")
 (dolist (charset '(kana han cjk-misc bopomofo))
   (set-fontset-font (frame-parameter nil 'font)
                     charset
-                    (font-spec :family "STXihei" :size 18)))
+                    (font-spec :family "WenQuanYi Micro Hei Mono" :size 16)))
+
 ;; change font size
 (global-set-key (kbd "C-M-=") 'text-scale-increase)
 (global-set-key (kbd "C-M--") 'text-scale-decrease)
