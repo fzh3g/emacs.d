8255b3233b28744cc1430456e17efa47b46cc269
Faxiang Zheng <fxzheng0906@outlook.com>
2016-05-07 02:46:25 +0800

    indent

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -17,7 +17,7 @@
       ;; if we need use different dictionary, we specify it in command line arguments
       (setq ispell-local-dictionary "en_US")
       (setq ispell-local-dictionary-alist
-                    '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8))))
+            '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8))))
      (t (setq ispell-program-name nil)))))
 
 (provide 'init-spelling)
