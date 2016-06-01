3eb37e22a33ae9a9cd8485bca706d24bffdfa72f
Faxiang Zheng <fxzheng0906@outlook.com>
2016-06-01 15:58:57 +0800

    clean code

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -13,38 +13,38 @@
           LaTeX-syntactic-comments t
           TeX-save-query nil
           reftex-plug-into-AUCTeX t)
-    (setq TeX-command-extra-options "-shell-escape"))
+    (setq TeX-command-extra-options "-shell-escape")
 
-  ;; default viewer
-  (when *linux*
-    (cond
-     ((executable-find "okular")
-      (setq TeX-view-program-selection
-            '((output-pdf "Okular")
-              (output-dvi "Okular"))))
-     ((executable-find "evince")
-      (setq TeX-view-program-selection
-            '((output-pdf "Evince")
-              (output-dvi "Evince"))))
-     (t
-      (setq TeX-view-program-selection
-            '((output-pdf "xdg-open")
-              (output-dvi "xdg-open"))))))
+    ;; default viewer
+    (when *linux*
+      (cond
+       ((executable-find "okular")
+        (setq TeX-view-program-selection
+              '((output-pdf "Okular")
+                (output-dvi "Okular"))))
+       ((executable-find "evince")
+        (setq TeX-view-program-selection
+              '((output-pdf "Evince")
+                (output-dvi "Evince"))))
+       (t
+        (setq TeX-view-program-selection
+              '((output-pdf "xdg-open")
+                (output-dvi "xdg-open"))))))
 
-  (add-hook 'LaTeX-mode-hook
-            (lambda ()
-              (imenu-add-menubar-index)
-              (LaTeX-math-mode t)
-              (flycheck-mode -1)
-              (reftex-mode t)))
+    (add-hook 'LaTeX-mode-hook
+              (lambda ()
+                (imenu-add-menubar-index)
+                (LaTeX-math-mode t)
+                (flycheck-mode -1)
+                (reftex-mode t)))
 
-  (add-hook 'TeX-mode-hook
-            (lambda ()
-              (outline-minor-mode t)
-              (flyspell-mode t)
-              (TeX-interactive-mode t)
-              (TeX-PDF-mode t)
-              (TeX-fold-mode t)
-              (visual-line-mode t))))
+    (add-hook 'TeX-mode-hook
+              (lambda ()
+                (outline-minor-mode t)
+                (flyspell-mode t)
+                (TeX-interactive-mode t)
+                (TeX-PDF-mode t)
+                (TeX-fold-mode t)
+                (visual-line-mode t)))))
 
 (provide 'init-latex)
