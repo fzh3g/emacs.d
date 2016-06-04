65c1b1e0fcc78aff7b5491103711f38a5c1cf4ad
Faxiang Zheng <fxzheng0906@outlook.com>
2016-06-04 21:06:30 +0800

    emoji fonts ❤️

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -4,6 +4,23 @@
   (set-fontset-font (frame-parameter nil 'font)
                     charset
                     (font-spec :family "WenQuanYi Micro Hei Mono" :size 16)))
+
+(defun --set-emoji-font (frame)
+  "Adjust the font settings of FRAME so Emacs can display emoji properly."
+  (if (eq system-type 'darwin)
+      ;; For NS/Cocoa
+      (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji")
+                        frame 'prepend)
+    ;; For Linux
+    (set-fontset-font t 'symbol (font-spec :family "Noto Emoji")
+                      frame 'prepend)))
+
+;; For when Emacs is started in GUI mode:
+(--set-emoji-font nil)
+;; Hook for when a frame is created with emacsclient
+;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
+(add-hook 'after-make-frame-functions '--set-emoji-font)
+
 (global-set-key (kbd "C-M-=") 'text-scale-increase)
 (global-set-key (kbd "C-M--") 'text-scale-decrease)
 (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
