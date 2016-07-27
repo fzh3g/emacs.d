2e16a9409fee62b865bcd54082728ccfef1639c1
Faxiang Zheng <fxzheng0906@outlook.com>
2016-07-28 00:27:49 +0800

    sudo save

--- lisp/init-tramp.el
+++ lisp/init-tramp.el
@@ -27,9 +27,7 @@
 ;; sudo save
 (defun sudo-save ()
   (interactive)
-  (if (not buffer-file-name)
-      (write-file (concat "/sudo:root@localhost:" (ido-read-file-name "File:")))
-    (write-file (concat "/sudo:root@localhost:" buffer-file-name))))
+  (write-file (concat "/sudo:root@localhost:" buffer-file-name)))
 (global-set-key (kbd "C-x M-s") #'sudo-save)
 
 (provide 'init-tramp)
