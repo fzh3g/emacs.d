51d004377084317958cd5f3874fedddc0890385d
Faxiang Zheng <fxzheng0906@outlook.com>
2016-07-29 06:01:57 +0800

    center-buffer disable diff-hl

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -117,6 +117,7 @@
 (global-set-key (kbd "C-x w m") #'fx/toggle-maximize-buffer)
 
 ;; http://emacs-doctor.com/emacs-strip-tease.html
+(defvar fx/center-buffer-diff-hl-mode-on-p nil)
 (define-minor-mode fx-centered-buffer-mode
   "Minor mode to use big fringe in the current buffer."
   :global t
@@ -126,14 +127,18 @@
       (progn
         (window-configuration-to-register ?_)
         (delete-other-windows)
-        (diff-hl-mode -1)
+        (when (boundp 'diff-hl-mode)
+          (setq fx/center-buffer-diff-hl-mode-on-p diff-hl-mode)
+          (when diff-hl-mode (diff-hl-mode -1)))
+        (setq indicate-empty-lines nil)
         (set-fringe-mode
          (/ (- (frame-pixel-width)
                (* 100 (frame-char-width)))
             2)))
     (set-fringe-style nil)
     (when (assoc ?_ register-alist)
-      (diff-hl-mode)
+      (when fx/center-buffer-diff-hl-mode-on-p (diff-hl-mode))
+      (setq indicate-empty-lines t)
       (jump-to-register ?_))))
 (global-set-key (kbd "C-x w c") #'fx-centered-buffer-mode)
 
