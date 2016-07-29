2a7e73b2e6491e28a1b48e3d0fd3f316b47ebcb1
Faxiang Zheng <fxzheng0906@outlook.com>
2016-07-30 02:39:04 +0800

    hide mode line mode

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -137,5 +137,30 @@
       (delete-other-windows))))
 (global-set-key (kbd "<f12>") #'fx/toggle-maximize-buffer)
 
+;; from http://emacs-doctor.com/emacs-hide-mode-line.html
+(defvar-local hidden-mode-line-mode nil)
+(defvar-local hide-mode-line nil)
+(define-minor-mode hidden-mode-line-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global t
+  :variable hidden-mode-line-mode
+  :group 'editing-basics
+  (if hidden-mode-line-mode
+      (setq hide-mode-line mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format hide-mode-line
+          hide-mode-line nil))
+  (force-mode-line-update)
+  ;; Apparently force-mode-line-update is not always enough to
+  ;; redisplay the mode-line
+  (redraw-display)
+  (when (and (called-interactively-p 'interactive)
+             hidden-mode-line-mode)
+    (run-with-idle-timer
+     0 nil 'message
+     (concat "Hidden Mode Line Mode enabled.  "
+             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+
 (provide 'init-gui-frames)
 ;;; init-gui-frames.el ends here
