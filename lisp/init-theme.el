37bf6ee1afd6b94d5eed27f29decebe17906a87a
Faxiang Zheng <fxzheng0906@outlook.com>
2016-07-31 03:00:27 +0800

    disable all themes before load a new one

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -15,6 +15,12 @@
 
 ;;; Code:
 
+;; @see https://plus.google.com/106672400078851000780/posts/KhTgscKE8PM
+(defadvice load-theme (before disable-themes-first activate)
+  "Diable all themes before load a new one."
+  (dolist (i custom-enabled-themes)
+    (disable-theme i)))
+
 (use-package leuven-theme
   :config
   (load-theme 'leuven t))
