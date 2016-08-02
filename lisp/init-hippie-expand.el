ed840f3b9a8f4ededef703315437443a31195dc5
Faxiang Zheng <fxzheng0906@outlook.com>
2016-08-02 10:41:56 +0800

    clean code

--- lisp/init-hippie-expand.el
+++ lisp/init-hippie-expand.el
@@ -15,19 +15,22 @@
 
 ;;; Code:
 
-(global-set-key (kbd "M-/") 'hippie-expand) ;; replace dabbrev-expand
-(setq
- hippie-expand-try-functions-list
- '(try-expand-dabbrev
-   try-expand-dabbrev-all-buffers
-   try-expand-dabbrev-from-kill
-   try-complete-file-name-partially
-   try-complete-file-name
-   try-expand-all-abbrevs
-   try-expand-list
-   try-expand-line
-   try-complete-lisp-symbol-partially
-   try-complete-lisp-symbol))
+(use-package hippie-exp
+  :bind ("M-/" . hippie-expand)
+  :config
+  (progn
+    (setq
+     hippie-expand-try-functions-list
+     '(try-expand-dabbrev
+       try-expand-dabbrev-all-buffers
+       try-expand-dabbrev-from-kill
+       try-complete-file-name-partially
+       try-complete-file-name
+       try-expand-all-abbrevs
+       try-expand-list
+       try-expand-line
+       try-complete-lisp-symbol-partially
+       try-complete-lisp-symbol))))
 
 (provide 'init-hippie-expand)
 ;;; init-hippie-expand.el ends here
