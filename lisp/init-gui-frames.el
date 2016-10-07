c288676f1f624cc5f4c289289b6e9ccb816110ad
Faxiang Zheng <fxzheng0906@outlook.com>
2016-10-08 03:15:49 +0800

    scroll

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -32,8 +32,7 @@
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 (when (fboundp 'set-scroll-bar-mode)
-  (unless *is-a-mac*
-    (scroll-bar-mode -1)))
+  (scroll-bar-mode -1))
 (when (fboundp 'horizontal-scroll-bar-mode)
   (horizontal-scroll-bar-mode -1))
 (when (fboundp 'menu-bar-mode)
