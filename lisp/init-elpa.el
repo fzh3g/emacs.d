eac1bddb8efa6c8781679c12319c53dad0699a29
Faxiang Zheng <fxzheng0906@outlook.com>
2016-11-13 23:44:26 +0800

    better to use when

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -130,7 +130,7 @@
 
 (setq fx-packages (append fx-packages fx-theme-packages))
 
-(if *is-a-mac*
+(when *is-a-mac*
   (add-to-list 'fx-packages 'exec-path-from-shell))
 
 (defvar fx-packages-all-installed-p t
