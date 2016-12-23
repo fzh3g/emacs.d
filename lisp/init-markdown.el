754cf5e4b9175448ae8aa7950f52f5a6adfa2bf9
Faxiang Zheng <fxzheng0906@outlook.com>
2016-12-24 04:36:05 +0800

    mmm-mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -77,29 +77,86 @@
     ))
 
 (use-package mmm-mode
-  :commands mmm-parse-buffer
+  :diminish mmm-mode
+  :commands mmm-mode
+  :init
+  (add-hook 'markdown-mode-hook
+            (lambda ()
+              (mmm-mode 1)))
   :config
   (progn
-    ;; http://jblevins.org/log/mmm
-    (defun my-mmm-markdown-auto-class (lang &optional submode)
-      "Define a mmm-mode class for LANG in `markdown-mode' using SUBMODE.
-If SUBMODE is not provided, use `LANG-mode' by default."
-      (let ((class (intern (concat "markdown-" lang)))
-            (submode (or submode (intern (concat lang "-mode"))))
-            (front (concat "^```" lang "[\n\r]+"))
-            (back "^```"))
-        (mmm-add-classes (list (list class :submode submode :front front :back back)))
-        (mmm-add-mode-ext-class 'markdown-mode nil class)))
-
-    ;; Mode names that derive directly from the language name
-    (mapc 'my-mmm-markdown-auto-class
-          '("awk" "bibtex" "c" "cpp" "java" "css" "latex" "html" "lisp" "makefile"
-            "markdown" "python" "r"))
-
-    ;; Mode names that differ from the language name
-    (my-mmm-markdown-auto-class "fortran" 'f90-mode)
-    (my-mmm-markdown-auto-class "elisp" 'emacs-lisp-mode)
-    (my-mmm-markdown-auto-class "shell" 'shell-script-mode)))
+    (mmm-add-classes '((markdown-ini
+                        :submode conf-unix-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```ini[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-python
+                        :submode python-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```python[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-html
+                        :submode web-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```html[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-java
+                        :submode java-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```java[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-ruby
+                        :submode ruby-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```ruby[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-c
+                        :submode c-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```c[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-c++
+                        :submode c++-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```c\+\+[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-elisp
+                        :submode emacs-lisp-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```elisp[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-javascript
+                        :submode javascript-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```javascript[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-ess
+                        :submode R-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```{?r.*}?[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-f90
+                        :submode f90-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```f90[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-classes '((markdown-idl
+                        :submode idlwave-mode
+                        :face mmm-declaration-submode-face
+                        :front "^```idl[\n\r]+"
+                        :back "^```$")))
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-ini)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-python)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-java)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-ruby)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-c)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-c++)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-elisp)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-html)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-javascript)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-ess)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-f90)
+    (mmm-add-mode-ext-class 'markdown-mode nil 'markdown-idl)))
 
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
