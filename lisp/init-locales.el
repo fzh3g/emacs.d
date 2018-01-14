3a32e7dff2c7da1bb6de8eec58a4df8706af84d6
Faxiang Zheng <fxzheng0906@outlook.com>
2018-01-14 09:11:46 +0800

    encoding

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -16,18 +16,18 @@
 
 ;;; Code:
 
-(defun sanityinc/utf8-locale-p (v)
-  "Return whether locale string V relates to a utf-8 locale."
-  (and v (string-match "utf-8" v)))
+(defun fx/utf8-locale-p (v)
+  "Return whether locale string V relates to a UTF-8 locale."
+  (and v (string-match "UTF-8" v)))
 
-(defun locale-is-utf8-p ()
-  "Return t if the \"locale\" command or environment variables prefer utf-8."
+(defun fx/locale-is-utf8-p ()
+  "Return t if the \"locale\" command or environment variables prefer UTF-8."
   (or (sanityinc/utf8-locale-p (and (executable-find "locale") (shell-command-to-string "locale")))
       (sanityinc/utf8-locale-p (getenv "LC_ALL"))
       (sanityinc/utf8-locale-p (getenv "LC_CTYPE"))
       (sanityinc/utf8-locale-p (getenv "LANG"))))
 
-(when (or window-system (locale-is-utf8-p))
+(when (or window-system (fx/locale-is-utf8-p))
   (setq locale-coding-system 'utf-8)
   (set-default-coding-systems 'utf-8)
   (set-terminal-coding-system 'utf-8)
