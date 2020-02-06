b3855016037ff39ba1cac7b2e948affd4a939753
Faxiang Zheng <fxzheng0906@outlook.com>
2020-02-06 22:11:10 +0800

    Revert "string-match-p"
    This reverts commit 028b5bca61e1bc09cf3272fa9933ab944a5618fa.

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -18,7 +18,7 @@
 
 (defun fx/utf8-locale-p (v)
   "Return whether locale string V relates to a UTF-8 locale."
-  (and v (string-match-p "UTF-8" v)))
+  (and v (string-match "UTF-8" v)))
 
 (defun fx/locale-is-utf8-p ()
   "Return t if the \"locale\" command or environment variables prefer UTF-8."
